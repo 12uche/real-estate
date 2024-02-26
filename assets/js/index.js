fdc4a98a1fa6d180779085da97025651e0b0f0b6
12uche <114584770+12uche@users.noreply.github.com>
2024-02-26 21:52:38 +0100

    real Esate

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,11 @@
-function toggleNav() {
-   var sidenav = document.getElementById("sidenav");
-   var main = document.querySelector(".main");
-   var menuIcon = document.getElementById("menuIcon");
- 
-   if (sidenav.style.width === "250px") {
-     sidenav.style.width = "0";
-     main.style.marginLeft = "0";
-     menuIcon.classList.remove("fa-times");
-     menuIcon.classList.add("fa-bars");
-   } else {
-     sidenav.style.width = "250px";
-     main.style.marginLeft = "250px";
-     menuIcon.classList.remove("fa-bars");
-     menuIcon.classList.add("fa-times");
-   }
-}
-   var links = document.querySelectorAll(".sidenav a");
-links.forEach(function(link) {
-   link.addEventListener("click", function() {
-      links.forEach(function(item) {
-      item.classList.remove("active");
-   });
-   link.classList.add("active");
-   });
-});
+// const burger = document.getElementById("burger-menu");
+// const sideNav = document.querySelector(".side-nav");
+// const closeBtn = document.querySelector(".close-btn");
+// const sidemenu = document.querySelectorAll(".side-nav a");
+
+// burger.addEventListener("click", () => {
+//   sideNav.classList.add("active");
+// });
+// closeBtn.addEventListener("click", () => {
+//   sideNav.classList.remove("active");
+// });
