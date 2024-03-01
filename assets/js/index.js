267ba5464d85724a656c73ed4046ff8c1b0300d7
12uche <114584770+12uche@users.noreply.github.com>
2024-03-01 11:09:47 +0100

    real-estate

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,23 @@
-// const burger = document.getElementById("burger-menu");
-// const sideNav = document.querySelector(".side-nav");
-// const closeBtn = document.querySelector(".close-btn");
-// const sidemenu = document.querySelectorAll(".side-nav a");
+let canSlide = true;
 
-// burger.addEventListener("click", () => {
-//   sideNav.classList.add("active");
-// });
-// closeBtn.addEventListener("click", () => {
-//   sideNav.classList.remove("active");
-// });
+document.getElementById('next').onclick = function () {
+   let list = document.querySelectorAll('.item');
+   document.getElementById('slide').appendChild(list[0]);
+   canSlide = false;
+}
+document.getElementById('prev').onclick = function () {
+   let list = document.querySelectorAll('.item');
+   document.getElementById('slide').prepend(list[list.length - 1]);
+   canSlide = false;
+}
+
+setInterval(() => {
+   if (!canSlide) {
+      canSlide = true;
+      return;
+   }
+   (() => {
+      let list = document.querySelectorAll('.item');
+      document.getElementById('slide').appendChild(list[0]);
+   })();
+}, 5000);
